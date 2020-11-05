67588a2a3dd37a63884dc1c2b8e62d780eba3192
Running water <alsgks1218@naver.com>
2020-11-05 23:50:13 +0900

    .

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,104 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
-export default () => <h1>Detail</h1>;
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      title
+      medium_cover_image
+      language
+      rating
+      description_intro
+    }
+    suggestions(id: $id) {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+`;
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  justify-items: center;
+  grid-gap: 25px;
+  width: 100%;
+  position: relative;
+  top: -50px;
+`;
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: Number(id) },
+  });
+  console.log("data is", data);
+  return (
+    <>
+      <Container>
+        <Column>
+          <Title>{loading ? "Loading ...." : data.movie.title}</Title>
+          {!loading && data.movie && (
+            <>
+              <Subtitle>
+                {data.movie.language} , {data.movie.rating}
+              </Subtitle>
+              <Description>{data.movie.description_intro}</Description>
+            </>
+          )}
+        </Column>
+        <Poster
+          bg={data && data.movie ? data.movie.medium_cover_image : ""}
+        ></Poster>
+      </Container>
+      {data?.suggestions && (
+        <Movies>
+          {data.suggestions.map((m) => (
+            <Movie key={m.id} id={m.id} bg={m.medium_cover_image}></Movie>
+          ))}
+        </Movies>
+      )}
+    </>
+  );
+};
